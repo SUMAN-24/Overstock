b354135ed2b32035a42888844921d3846a5951c9
SUMAN-24 <khansuman240694@gmail.com>
2022-05-07 20:46:06 +0530

    Import Export function added
    Implemented Import Export. And made minor changes and updated

---
+++ components/footer.js
@@ -0,0 +1,53 @@
+function footer() {
+    return `
+    <div class="foot">
+    <div class="ft">
+        <div>MY ACCOUNT</div>
+        <span>Order & Returns</span>
+        <span>Email Preferences</span>
+        <span>Account Settings</span>
+    </div>
+    <div class="ft">
+        <div>LET US HELP</div>
+        <span>Contact Customer Care</span>
+        <span>Shipping Information</span>
+        <span>Return Policy</span>
+        <span>International Help</span>
+        <span>Accessibility</span>
+    </div>
+    <div class="ft">
+        <div>COMPANY INFORMATION</div>
+        <span>About Overstock™</span>
+        <span>Contact Us</span>
+        <span>Careers</span>
+        <span>Investor Relations</span>
+        <span>Sell Your Products</span>
+        <span>Supply Chain Transparency</span>
+    </div>
+    <div class="ft">
+        <div>MORE WAYS TO SHOP</div>
+        <span>Tips & Ideas</span>
+        <span>Deals</span>
+        <span>Clearance</span>
+        <span>New Arrivals</span>
+    </div>
+</div>
+<div class="footer2">
+    <div class="footer2_0">
+        <div class="footer2_1">
+            <p>© Copyright 2022, Overstock.com®, Inc.</p>
+            <p>799 Coliseum Way Midvale, UT 84047 | 1-800-843-2446</p>
+        </div>
+        <div class="footer2_2">
+            <span>Privacy Policy</span>
+            <span>Terms & Conditions</span>
+            <span>Promotion Terms</span>
+            <span>Ship to:</span>
+            <img src="	https://ak1.ostkcdn.com/img/mxc/intFlag_IN.gif" alt="Ship to">
+        </div>
+    </div>    
+</div>
+    `
+}
+
+export {footer};
---
+++ components/navbar.js
@@ -0,0 +1,72 @@
+function navbar() {
+    return `
+    <div class="ships">
+        <div>
+            Ships to:
+        </div>
+        <a href="#" class="country">
+            <img src="https://ak1.ostkcdn.com/img/mxc/intFlag_IN.gif" 
+            title="Click to change Country or Currency" alt="India flag" 
+            class="FlagPic">
+            
+            <div class="country_name">India</div>
+        </a>
+    </div>
+    <div>
+        <div class="liner"></div>
+        <div class="userbar">
+            <a href="/index.html">
+            <img src="/images/overstock.com_1516987437.png" alt="overstock logo">
+            </a>
+            <div >
+                <input oninput="debounce(main,500)" type="text" placeholder="Search">
+            </div>
+            <button aria-label="search" type="submit" class="search_butn">
+                <svg width="22" height="22" viewBox="0 0 24 24" stroke="#2F3337" fill="#FFFFFF">
+                <title>Search</title>
+                <path d="M22 22l-6.344-6.344M10 18a8 8 0 100-16 8 8 0 000 16z" stroke="inherit" fill="none" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round">
+                </path>
+                </svg>
+            </button>
+            <div class="user_details">
+                <div class="user_details1">
+                    <i class="fa-regular fa-user fa-customize"></i>
+                    <p title="User">Account</p>
+                </div>
+                <div class="user_details1">
+                    <i class="fa-regular fa-heart fa-customize"></i>
+                    <p title="Heart">Lists</p>
+                </div>
+                <div class="user_details1">    
+                    <i class="fa-solid fa-cart-shopping fa-customize"></i>
+                    <p title="Cart Empty">Cart</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="liner1"></div>
+        <div class="navbars">
+            <div class="navbar_contents">
+                <a href="#">Furniture</a>
+                <a href="#">Rugs</a>
+                <a href="#">Decor</a>
+                <a href="#">Bed & Bath</a>
+                <a href="#">Home Improvement</a>
+                <a href="#">Kitchen</a>
+                <a href="#">Outdoor</a>
+                <a href="#">Jewelry</a>
+                <a href="#">Lighting</a>
+                <a href="#">Kids & Baby</a>
+                <a href="#">More</a>
+                <p style="color:rgb(235,236,237)">|</p>
+                <a href="#">Ideas</a>
+                <a href="#" style="color:rgb(211,90,98)">Sales & Deals</a>
+            </div>
+        </div>
+        <div class="liner2"></div>
+    </div>
+    `
+}
+
+export {navbar};
